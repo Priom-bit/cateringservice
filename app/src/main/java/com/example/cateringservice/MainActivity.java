86f0af37c8b2f09974b9483f5bcca9c85dab04a6
priom <arpriom@gmail.com>
2020-09-25 01:40:16 +0600

    basic functionality

--- app/src/main/java/com/example/cateringservice/MainActivity.java
+++ app/src/main/java/com/example/cateringservice/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.cateringservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginbuttonid);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Hey! you click me!!!");
+            }
+        });
     }
 }
